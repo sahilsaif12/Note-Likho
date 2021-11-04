94c66ae350e71a24cd53068daf55e322854475df
sk saifuddin <91179915+sahilsaif12@users.noreply.github.com>
2021-11-04 22:50:32 +0530

    bcryptjs and jsonwebtoken package uses on auth.js

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,42 +2,53 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// Create a user using: POST "/api/auth/createuser". Doesn't required Authentication
+
+const JWT_SECRET='SahilKeep#pati$ence'
+/// Create a user using: POST "/api/auth/createuser". Doesn't required Authentication
 router.post("/createuser", [
   body('name', "enter a valid name").isLength({ min: 3 }),
   body('email', "enter a valid email").isEmail(),
   body('password', "password should be at least 6 character").isLength({ min: 6 }),
 ], async (req, res) => {
-  // if there are errors, return bad request and the errors
-  const errors = validationResult(req);
+
+  /// if there are errors, return bad request and the errors
+  const errors = validationResult(req); 
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  
-  // check whether the user with same email exists already
+
+  /// check whether the user with same email exists already
   try {
+
+
+    let user = await User.findOne({ email: req.body.email })
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user is already exist with this email" })
+    }
+
+    /// create a new user
+    const salt =await bcrypt.genSalt(10)
+    securePassword =await bcrypt.hash(req.body.password,salt)
+    let user2 = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: securePassword,
+    })
     
-  
-  let user=await User.findOne({email:req.body.email})
-  if (user) {
-    return res.status(400).json({error:"Sorry a user is already exist with this email"})
-  }
-  
-  let user2 = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-  res.json(user2)
-} catch (error) {
+    const data={
+      user:{
+        id:user2.id
+      }
+    }
+    const authToken=jwt.sign(data,JWT_SECRET)
+    res.json({authToken})
+  } catch (error) {
     console.log(error.message);
+    res.status(500).send("some error accrued")
   }
-  // .then(user => res.json(user))
-  // .catch(err=>{
-  //   console.log(err)
-  //   res.json({error:'The email is already used , use another email id',message:err.message})
-  // })
 
 })
 
