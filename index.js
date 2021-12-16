2b399dc98a26100807c5951474e174781327ce8c
sk saifuddin <91179915+sahilsaif12@users.noreply.github.com>
2021-12-16 20:24:59 +0530

    dspwl

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api/notes', require('./routes/notes'))
 // serve static assets if in production
 if (process.env.NODE_ENV ==='production') {
   // set static folder
-  app.use(express.static('client/build'))
+  app.use(express.static(path.resolve('./client/build')))
   app.get('/*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./Client/build','index.html'))
   })
