8377d006007ad5af8918e890d85aedfb1ad57aa1
sk saifuddin <91179915+sahilsaif12@users.noreply.github.com>
2021-12-16 15:39:57 +0530

    ds z

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/notes', require('./routes/notes'))
 if (process.env.NODE_ENV ==='production') {
   // set static folder
   app.use(express.static('client/build'))
-  app.get('*',(req,res)=>{
+  app.get('/*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'Client','build','index.html'))
   })
   
